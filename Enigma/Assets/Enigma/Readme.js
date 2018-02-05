8b2c4bdf03be1c3e5ad9ddf807f0d3f85be05979
david.zetterdahl <david.zetterdahl@hotmail.com>
2018-02-05 22:57:32 +0100

    Project created
    Enabled multiplayer, added some folder structures. More to come in a
    bit.

---
+++ Enigma/Assets/Enigma/Readme.js
@@ -0,0 +1,7 @@
+﻿/*
+Folder structure:
+Should probably read up on this sometime.
+
+Will look back at this file in a year or two and agree and then continue with our shitty structure system.
+
+*/
---
+++ Enigma/Assets/Readme.js
@@ -0,0 +1,8 @@
+﻿/*
+
+I didn't see a way to create a .txt so here goes:
+
+Drop EVERYTHING that is made by us in the Engima folder, that we if anyone downloads a third party asset, it will be in their own folder
+and we will be able to keep track of what stuff is ours. Also make lots of folders. Folders make people happy.
+
+*/
