1d2fa08c1cd14efed544ef0ed13455564fe31d2f
jjjlalonde@gmail.com <jjjlalonde@gmail.com>
2018-09-16 12:55:42 -0400

    Think I did this right

---
+++ Enigma/Assets/Enigma/Components/Base Classes/Weapons/Calculator/CalculatorReloader.cs
@@ -0,0 +1,18 @@
+﻿namespace Assets.Enigma.Components.Base_Classes.Weapons.Calculator
+{
+    public class CalculatorReloader : vp_WeaponReloader
+    {
+        protected override void Start()
+        {
+            InvokeRepeating("Reload", 0, 2.0f);
+        }
+
+        private void Reload()
+        {
+            if (m_Player.CurrentWeaponAmmoCount.Get() < m_Player.CurrentWeaponMaxAmmoCount.Get())
+            {
+                m_Player.CurrentWeaponAmmoCount.Set(m_Player.CurrentWeaponAmmoCount.Get() + 1);
+            }
+        }
+    }
+}
