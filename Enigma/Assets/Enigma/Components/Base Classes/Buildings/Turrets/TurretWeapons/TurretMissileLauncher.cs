b580e3e7a29854575bfc40349c86cc4408f27963
lorddz <david_zetterdahl@hotmail.com>
2019-03-28 21:31:42 +0100

    Removed and uncommented code that pointed towards the old network manager and thus prevented the project from building.

--- Enigma/Assets/Enigma/Components/Base Classes/Buildings/Turrets/TurretWeapons/TurretMissileLauncher.cs
+++ Enigma/Assets/Enigma/Components/Base Classes/Buildings/Turrets/TurretWeapons/TurretMissileLauncher.cs
@@ -1,52 +1,52 @@
-﻿using System.Collections;
-using Enigma.Components.Base_Classes.Buildings.Turrets.Targeting;
-using Enigma.Components.Base_Classes.Launchables;
-using UnityEngine;
+﻿//using System.Collections;
+//using Enigma.Components.Base_Classes.Buildings.Turrets.Targeting;
+//using Enigma.Components.Base_Classes.Launchables;
+//using UnityEngine;
 
-namespace Enigma.Components.Base_Classes.Buildings.Turrets.TurretWeapons
-{
-    public class TurretMissileLauncher : MonoBehaviour, ITurretWeapon
-    {
-        public GameObject MissilePrefab;
-        public GameObject MissileSpawnPoint;
-        public Collider TurretCollider;
-        public int ReloadTime;
-        private bool _reloading;
+//namespace Enigma.Components.Base_Classes.Buildings.Turrets.TurretWeapons
+//{
+//    public class TurretMissileLauncher : MonoBehaviour, ITurretWeapon
+//    {
+//        public GameObject MissilePrefab;
+//        public GameObject MissileSpawnPoint;
+//        public Collider TurretCollider;
+//        public int ReloadTime;
+//        private bool _reloading;
 
-        public void FixedUpdate()
-        {
-            var target = GetComponent<ITargeter>().Target;
-            if (target != null)
-            {
-                TurretCollider.transform.LookAt(target.transform);
-            }
-        }
+//        public void FixedUpdate()
+//        {
+//            var target = GetComponent<ITargeter>().Target;
+//            if (target != null)
+//            {
+//                TurretCollider.transform.LookAt(target.transform);
+//            }
+//        }
 
-        public void Attack(GameObject target)
-        {
-            FireMissilesAtTarget(target);
-        }
+//        public void Attack(GameObject target)
+//        {
+//            FireMissilesAtTarget(target);
+//        }
 
-        private void FireMissilesAtTarget(GameObject target)
-        {
-            if (_reloading)
-            {
-                return;
-            }
+//        private void FireMissilesAtTarget(GameObject target)
+//        {
+//            if (_reloading)
+//            {
+//                return;
+//            }
 
-            var missile = Instantiate(MissilePrefab, MissileSpawnPoint.transform.position,
-                                      MissileSpawnPoint.transform.rotation);
-            missile.GetComponent<HomingMissle>().SetTarget(target);
-            missile.GetComponent<HomingMissle>().Start();
-            missile.gameObject.SetActive(true);
-            StartCoroutine(Reload());
-        }
+//            var missile = Instantiate(MissilePrefab, MissileSpawnPoint.transform.position,
+//                                      MissileSpawnPoint.transform.rotation);
+//            missile.GetComponent<HomingMissle>().SetTarget(target);
+//            missile.GetComponent<HomingMissle>().Start();
+//            missile.gameObject.SetActive(true);
+//            StartCoroutine(Reload());
+//        }
 
-        private IEnumerator Reload()
-        {
-            _reloading = true;
-            yield return new WaitForSeconds(ReloadTime);
-            _reloading = false;
-        }
-    }
-}
+//        private IEnumerator Reload()
+//        {
+//            _reloading = true;
+//            yield return new WaitForSeconds(ReloadTime);
+//            _reloading = false;
+//        }
+//    }
+//}
